4cbd2115e102e946c9ada02dd3f12b85d44be4b0
eriksgda <erik.ksgda@gmail.com>
2026-02-02 20:29:59 -0300

    feat: add search_logs database schema

---
+++ database/migrations/2026_02_02_184247_search_logs.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('search_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('search_text');
+            $table->string('ip_address', 45);
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('search_logs');
+    }
+};
