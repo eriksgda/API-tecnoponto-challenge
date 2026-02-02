45832ce52114fbcc6873e6dd9dc74585b4f8349c
eriksgda <erik.ksgda@gmail.com>
2026-02-02 20:33:27 -0300

    feat: add serach_log model

---
+++ app/Models/SearchLog.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SearchLog extends Model
+{
+    protected $table = 'search_logs';
+
+    protected $fillable = [
+        'search_text',
+        'ip_address'
+    ];
+}
