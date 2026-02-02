c36a86e73a2797a0d249db3fcd0148e8551fe9e5
eriksgda <erik.ksgda@gmail.com>
2026-02-02 20:35:29 -0300

    feat: add character search endpoint controller

---
+++ app/Http/Controllers/CharController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\ExternalApiService;
+use App\DTOs\CharResponseDTO;
+
+class CharController
+{
+    private ExternalApiService $serivce;
+
+    public function __construct(ExternalApiService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function show(Request $request)
+    {
+        $charName = $request->query('name');
+        $page = (int) $request->query('page', 1);
+
+        if (!$charName) {
+            return response()->json(['error' => 'Parâmetro name é obrigatório'], 400);
+        }
+
+        $response = $this->service->getCharByName($charName, $page);
+
+        return response()->json($response, 200);
+    }
+
+}
