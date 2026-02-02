5881830a9021e3f53c2670a2e16e2d9bae59be54
eriksgda <erik.ksgda@gmail.com>
2026-02-02 20:31:05 -0300

    feat: add episode dto

---
+++ app/DTOs/EpisodeDTO.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DTOs;
+
+class EpisodeDTO
+{
+
+    public function __construct(
+        public string $name,
+        public string $air_date,
+        public string $episode,
+    ){}
+
+        public static function fromApi(array $data): self
+    {
+        return new self(
+            name: $data['name'],
+            air_date: $data['air_date'],
+            episode: $data['episode'],
+        );
+    }
+}
