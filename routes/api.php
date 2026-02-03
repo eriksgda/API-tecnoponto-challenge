d0d6e7f20b07f1cfb09c9a9f3f8274fa2df7647d
eriksgda <erik.ksgda@gmail.com>
2026-02-02 21:27:12 -0300

    feat: add log endpoint route

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CharController;
+use App\Http\Controllers\LogController;
 
 Route::get('/personagens', [CharController::class, 'show']);
+
+Route::get('/logs', [LogController::class, 'index']);
